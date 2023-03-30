002d1b231a86bcfc1e440e869fcaa3b7b562e5a9
Vitoria Oliveira <vitoria.oli.san14@gmail.com>
2023-03-30 20:47:12 +0200

    create Home view

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+
+<header>
+    <img class="logo" src="../assets/imgs/logo.png" alt="logo">
+
+    <nav>
+        <button class="btn">Home</button>
+        <button class="btn">Obras</button>
+        <button class="btn">Recomendaciones</button>
+    </nav>
+</header>
+
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/Variables.scss";
+
+header{
+    background-color: $Details;
+    height: 22%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo{
+        width: 20vw;
+        margin-left: 2vw;
+    }
+
+    nav{
+
+        .btn{  
+            background-color: transparent;
+            border: none;
+            margin-right: 2vw;
+            height: 5vh;
+            font-family: Titles;
+            font-size: 2vw;
+            color: white;
+
+        }
+    }
+}
+
+</style>
---
+++ src/components/Poetry.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+<template>
+<div class="poems">
+    <div class="card">
+    <img class="background" src="../assets/imgs/fondo.jpg" alt="">
+    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro itaque nam. Harum, obcaecati? Libero quos at ab atque distinctio corporis? Dolorem vitae optio quia ipsam voluptatum odit necessitatibus eaque.</div>
+    </div>
+    <div class="card">
+    <img class="background" src="../assets/imgs/fondo.jpg" alt="">
+    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro itaque nam. Harum, obcaecati? Libero quos at ab atque distinctio corporis? Dolorem vitae optio quia ipsam voluptatum odit necessitatibus eaque.</div>
+    </div>
+    <div class="card">
+    <img class="background" src="../assets/imgs/fondo.jpg" alt="">
+    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro itaque nam. Harum, obcaecati? Libero quos at ab atque distinctio corporis? Dolorem vitae optio quia ipsam voluptatum odit necessitatibus eaque.</div>
+    </div>
+</div>
+</template>
+<style lang="scss" scoped>
+@import "../assets/scss/Variables.scss";
+
+.poems{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 2vw;
+    margin-bottom: 2vw;
+    
+    .card{
+        position: relative;
+        display: inline-block;
+        text-align: center;
+        .background{
+            width: 25vw;
+            opacity: 50%;
+
+        }
+        .text{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            font-family: Text;
+        }
+    }
+}
+
+</style>
